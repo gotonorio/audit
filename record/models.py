83759b8a85e8e23d7d5c71e0b3eb4329ea880519
gotonorio <gotonorio@gmail.com>
2024-04-27 10:41:23 +0900

    Ruffによるsouceの整理

--- record/models.py
+++ record/models.py
@@ -27,9 +27,7 @@ class Account(models.Model):
 
     account_name = models.CharField(verbose_name="口座名", max_length=32)
     branch_number = models.CharField(verbose_name="支店番号", max_length=10)
-    account_number = models.CharField(
-        verbose_name="口座番号", max_length=16, unique=True
-    )
+    account_number = models.CharField(verbose_name="口座番号", max_length=16, unique=True)
     account_type = models.CharField(verbose_name="口座種類", max_length=16)
     bank = models.ForeignKey(Bank, verbose_name="銀行名", on_delete=models.CASCADE)
     alive = models.BooleanField(verbose_name="有効", default=True)
@@ -53,9 +51,7 @@ class AccountingClass(models.Model):
     """会計区分マスタ（Kurasel導入で追加）"""
 
     code = models.IntegerField(verbose_name="コード", unique=True)
-    accounting_name = models.CharField(
-        verbose_name="会計区分名", max_length=32, unique=True
-    )
+    accounting_name = models.CharField(verbose_name="会計区分名", max_length=32, unique=True)
 
     def __str__(self):
         return self.accounting_name
@@ -102,9 +98,7 @@ class Himoku(models.Model):
     alive = models.BooleanField(verbose_name="有効", default=True)
     aggregate_flag = models.BooleanField(verbose_name="集計", default=True)
     # Kurasel導入で追加
-    accounting_class = models.ForeignKey(
-        AccountingClass, on_delete=models.CASCADE, blank=True, null=True
-    )
+    accounting_class = models.ForeignKey(AccountingClass, on_delete=models.CASCADE, blank=True, null=True)
     is_approval = models.BooleanField(verbose_name="承認必要", default=True)
     is_default = models.BooleanField(verbose_name="デフォルト", default=False)
 
@@ -118,9 +112,7 @@ class Himoku(models.Model):
         """
 
         constraints = [
-            models.UniqueConstraint(
-                fields=["himoku_name", "accounting_class"], name="himoku_unique"
-            ),
+            models.UniqueConstraint(fields=["himoku_name", "accounting_class"], name="himoku_unique"),
             models.UniqueConstraint(
                 fields=["is_default"],
                 condition=models.Q(is_default=True),
@@ -193,9 +185,7 @@ class Himoku(models.Model):
     def get_himoku_list(cls):
         """有効な費目名をリストで返す"""
         himoku_list = []
-        himoku_list = cls.objects.filter(alive=True).values_list(
-            "himoku_name", flat=True
-        )
+        himoku_list = cls.objects.filter(alive=True).values_list("himoku_name", flat=True)
         return himoku_list
 
     @classmethod
@@ -230,15 +220,9 @@ class Himoku(models.Model):
 class TransferRequester(models.Model):
     """Kuraselの振込依頼人データと費目名を関連付けるためのモデル"""
 
-    requester = models.CharField(
-        verbose_name="振込依頼人名", max_length=64, blank=True, null=True
-    )
-    himoku = models.ForeignKey(
-        Himoku, verbose_name="費目名", on_delete=models.CASCADE, blank=True, null=True
-    )
-    comment = models.CharField(
-        verbose_name="備考", max_length=64, blank=True, null=True
-    )
+    requester = models.CharField(verbose_name="振込依頼人名", max_length=64, blank=True, null=True)
+    himoku = models.ForeignKey(Himoku, verbose_name="費目名", on_delete=models.CASCADE, blank=True, null=True)
+    comment = models.CharField(verbose_name="備考", max_length=64, blank=True, null=True)
 
     def __str__(self):
         return self.requester
@@ -256,23 +240,15 @@ class Transaction(models.Model):
     入出金種別を「入金/出金」だけにするため、is_incomeフィールドを追加。2023-03-04
     """
 
-    account = models.ForeignKey(
-        Account, verbose_name="口座名", on_delete=models.CASCADE, null=True
-    )
+    account = models.ForeignKey(Account, verbose_name="口座名", on_delete=models.CASCADE, null=True)
     is_income = models.BooleanField(verbose_name="入金flg", default=False)
     transaction_date = models.DateField(verbose_name="取引日")
     ammount = models.IntegerField(verbose_name="金額", default=0)
-    himoku = models.ForeignKey(
-        Himoku, verbose_name="費目名", on_delete=models.CASCADE, null=True
-    )
-    requesters_name = models.CharField(
-        verbose_name="振込依頼人名", max_length=64, default=""
-    )
+    himoku = models.ForeignKey(Himoku, verbose_name="費目名", on_delete=models.CASCADE, null=True)
+    requesters_name = models.CharField(verbose_name="振込依頼人名", max_length=64, default="")
     description = models.CharField(verbose_name="摘要", max_length=64, default="")
     balance = models.IntegerField(verbose_name="残高", null=True, blank=True)
-    author = models.ForeignKey(
-        user, verbose_name="記録者", on_delete=models.CASCADE, null=True
-    )
+    author = models.ForeignKey(user, verbose_name="記録者", on_delete=models.CASCADE, null=True)
     created_date = models.DateTimeField(verbose_name="作成日", default=timezone.now)
     delete_flg = models.BooleanField(default=False)
     calc_flg = models.BooleanField(default=True)
@@ -283,7 +259,7 @@ class Transaction(models.Model):
     is_approval = models.BooleanField(verbose_name="承認必要", default=True)
 
     def __str__(self):
-        return self.himoku
+        return self.himoku.himoku_name
 
     # def delete(self):
     #     """ delete関数を論理削除にするためにオーバーライド
@@ -300,9 +276,7 @@ class Transaction(models.Model):
         # 前受金の費目id
         id = Himoku.get_himoku_obj(settings.MAEUKE, "管理")
         total = 0
-        qs = Transaction.objects.filter(transaction_date__range=[tstart, tend]).filter(
-            himoku=id
-        )
+        qs = Transaction.objects.filter(transaction_date__range=[tstart, tend]).filter(himoku=id)
         for i in qs:
             total += i.ammount
         return total
@@ -353,9 +327,7 @@ class Transaction(models.Model):
         if manualinput:
             qs_pb = qs_pb.filter(transaction_date__range=[tstart, tend])
         else:
-            qs_pb = qs_pb.filter(transaction_date__range=[tstart, tend]).filter(
-                is_manualinput=False
-            )
+            qs_pb = qs_pb.filter(transaction_date__range=[tstart, tend]).filter(is_manualinput=False)
         # 入金・出金のfilter
         if deposit_flg == "income":
             qs_pb = qs_pb.filter(is_income=True)
@@ -386,9 +358,7 @@ class Transaction(models.Model):
     #     return total_pb
 
     @classmethod
-    def dwd_from_kurasel(
-        cls, data, paymentmethod_list, requester_list, default_himoku
-    ) -> int:
+    def dwd_from_kurasel(cls, data, paymentmethod_list, requester_list, default_himoku) -> int:
         """kurasel_translatorからDeposits and withdrawals（入出金明細データ）を読み込む
         - 戻り値：取り込んだ「月」(int型)。エラーの場合は0を返す。
         - 種類、日付、金額、振り込み依頼人でget_or_createする。
@@ -489,12 +459,8 @@ class Transaction(models.Model):
 class ApprovalCheckData(models.Model):
     """入出金明細データの「支払い承認」が必要か否かを判定するための文字列オブジェクト"""
 
-    atext = models.CharField(
-        verbose_name="チェック文字列", max_length=16, blank=True, null=True, unique=True
-    )
-    comment = models.CharField(
-        verbose_name="備考", max_length=50, blank=True, null=True
-    )
+    atext = models.CharField(verbose_name="チェック文字列", max_length=16, blank=True, null=True, unique=True)
+    comment = models.CharField(verbose_name="備考", max_length=50, blank=True, null=True)
     alive = models.BooleanField(verbose_name="有効", default=True)
 
     def __str__(self):
@@ -583,9 +549,7 @@ class ClaimData(models.Model):
     def get_claim_list(cls, tstart, tend, claim_type):
         """管理費等の請求時「未収金」「前受金」「請求不備」のリストを返す"""
 
-        claim_qs = cls.objects.filter(claim_date__range=[tstart, tend]).filter(
-            claim_type=claim_type
-        )
+        claim_qs = cls.objects.filter(claim_date__range=[tstart, tend]).filter(claim_type=claim_type)
         total = 0
         for i in claim_qs:
             total += i.ammount
